4b73175567ab506a010ca4f102e424356edecad1
itggot-harald-hammarstrom <harald.hammarstrom@elev.ga.ntig.se>
2019-04-25 15:42:07 +0200

    nästan klar med förstasida

---
+++ site/js/main.js
@@ -0,0 +1,19 @@
+function meny(event){
+    let meny = document.querySelector(".burgarmeny")
+    meny.classList.toggle("activate")
+}
+
+function collapsible1(event){
+    let meny = document.querySelector(".collapsiblebox1")
+    meny.classList.toggle("activate")
+}
+
+function collapsible2(event){
+    let meny = document.querySelector(".collapsiblebox2")
+    meny.classList.toggle("activate")
+}
+
+function collapsible3(event){
+    let meny = document.querySelector(".collapsiblebox3")
+    meny.classList.toggle("activate")
+}
